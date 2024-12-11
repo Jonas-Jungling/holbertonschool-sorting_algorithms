32ec0574988785b8983dfbbc8b20ca5cad759665
Jonas-Jungling <jonas.jungling@oclock.school>
2024-12-11 09:59:22 +0100

    Add function selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,11 +10,16 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
+	size_t min_idx;
+	size_t temp;
+
+	if (!array || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 
 	{
-		size_t min_idx = i;
+		min_idx = i;
 
 		for (j = i + 1; j < size; j++)
 		{
@@ -24,11 +29,12 @@ void selection_sort(int *array, size_t size)
 
 			}
 		}
-
-		size_t temp = array[i];
-
-		array[i] = array[min_idx];
-		array[min_idx] = temp;
-		print_array(array, size);
+		if (i != min_idx)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
 	}
 }
